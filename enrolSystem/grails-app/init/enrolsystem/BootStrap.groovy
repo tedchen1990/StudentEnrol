092d007ac5098bb87109ebb3a4f715f9fad2544d
tedchen1990 <tedchenguojun@gmail.com>
2019-02-05 17:53:12 +0000

    05/02/2019

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,9 +1,52 @@
-package enrolsystem
+package com.enrol
 
 class BootStrap {
 
     def init = { servletContext ->
+	
+	def course1 = new Course
+	(
+	department: 'Computing3',
+	courseCode: 'CS123',
+	courseTitle: 'BSc Hon Computing',
+	courseLeader: 'Dr Michelle Murphy',
+	startDate:new Date('09/09/2019'),
+	endDate:new Date('07/07/2023'),
+	numberOfStudents: 55,
+	studyMode:'Fulltime',
+	tuitionFees:9000.60,
+	description : 'Lorem ipsum dolor sit amet...'	
+	).save()
+
+def course2 = new Course
+	(
+	department: 'Computing2',
+	courseCode: 'CS123',
+	courseTitle: 'BSc Hon Computing',
+	courseLeader: 'Dr Michelle Murphy',
+	startDate:new Date('09/09/2019'),
+	endDate:new Date('07/07/2023'),
+	numberOfStudents: 55,
+	studyMode:'Fulltime',
+	tuitionFees:9000.60,
+	description : 'Lorem ipsum dolor sit amet...'	
+	).save()
+
+def course3 = new Course
+	(
+	department: 'Computin3',
+	courseCode: 'CS123',
+	courseTitle: 'BSc Hon Computing',
+	courseLeader: 'Dr Michelle Murphy',
+	startDate:new Date('09/09/2019'),
+	endDate:new Date('07/07/2023'),
+	numberOfStudents: 55,
+	studyMode:'Fulltime',
+	tuitionFees:9000.60,
+	description : 'Lorem ipsum dolor sit amet...'	
+	).save()
     }
+
     def destroy = {
     }
 }
